14d1be21323b31b23c5d773f3d158c2b76e61900
Joshua Villing <jsvilling@gmail.com>
2019-05-21 19:24:49 +0200

    Add basic main class

---
+++ src/main/java/ch/fhnw/mada/jvi/huffman/MadaHuffmanAssignment.java
@@ -0,0 +1,11 @@
+package ch.fhnw.mada.jvi.huffman;
+
+import ch.fhnw.mada.jvi.huffman.ui.CommandLineInterface;
+
+import java.io.IOException;
+
+public class MadaHuffmanAssignment {
+    public static void main(String[] args) throws IOException {
+        new CommandLineInterface().run();
+    }
+}
