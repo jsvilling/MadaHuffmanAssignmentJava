c2ff52c69053ca8eda7b46e5fa38fe7c1fb5b5ca
Joshua Villing <jsvilling@gmail.com>
2019-05-24 17:44:58 +0200

    Add integration test for encoding

--- src/test/java/it/HuffanEncodingTest.java
+++ src/test/java/it/HuffanEncodingTest.java
@@ -13,11 +13,11 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 public class HuffanEncodingTest {
 
-    private static String sourceFile = "src/test/resources/it/text.txt";
-    private static String codeFile = "src/test/resources/it/source/dec_tab-mada.txt";
-    private static String compressedFile = "src/test/resources/it/source/output-mada.dat";
-    private static String decompressedFile = "src/test/resources/it/target/decompressed.txt";
-    private static String decompressedExpectedFile = "src/test/resources/it/expected/decompressedExpected.txt";
+    private static String sourceFile = "src/test/resources/it/decde/text.txt";
+    private static String codeFile = "src/test/resources/it/decode/dec_tab-mada.txt";
+    private static String compressedFile = "src/test/resources/it/decode/output-mada.dat";
+    private static String decompressedFile = "src/test/resources/it/decode/text.txt";
+    private static String decompressedExpectedFile = "src/test/resources/it/decode/decompressedExpected.txt";
 
     @Test
     public void testDecodingEncodedFromExerciseSheet() throws IOException {
@@ -32,6 +32,29 @@ public class HuffanEncodingTest {
         String actualDecoded = Files.readString(Paths.get(decompressedFile));
         String expectedDecoded = Files.readString(Paths.get(decompressedExpectedFile));
         assertThat(actualDecoded).isEqualTo(expectedDecoded);
+    }
+
+    @Test
+    public void testEncodingFromExerciseSheet() throws IOException {
+        // Given
+        String sourceFile = "src/test/resources/it/encode/text.txt";
+        String codeFile = "src/test/resources/it/encode/dec_tab-mada.txt";
+        String compressedFile = "src/test/resources/it/encode/output-mada.dat";
+        String decompressedFile = "src/test/resources/it/encode/text.txt";
+        String codeFileExpected = "src/test/resources/it/encode/dec_tab-expected.txt";
+
+        FileService fileService = new FileService(sourceFile, codeFile, compressedFile, decompressedFile);
+        EncodingService encodingService = new EncodingService(fileService, new CodeMapService());
+
+        Files.writeString(Paths.get(codeFile), "");
+
+        // When
+        encodingService.encode();
+
+        // Then
+        String actualCode = Files.readString(Paths.get(codeFile));
+        String expectedCode = Files.readString(Paths.get(codeFileExpected));
+        assertThat(actualCode).isEqualTo(expectedCode);
     }
 
 }
