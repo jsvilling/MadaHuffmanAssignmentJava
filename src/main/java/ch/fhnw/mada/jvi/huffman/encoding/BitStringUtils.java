63dcac6e09b4ddb34d68503390c42cea01e840fe
Joshua Villing <jsvilling@gmail.com>
2019-05-24 21:09:27 +0200

    Rewrite bit string creation

--- src/main/java/ch/fhnw/mada/jvi/huffman/encoding/BitStringUtils.java
+++ src/main/java/ch/fhnw/mada/jvi/huffman/encoding/BitStringUtils.java
@@ -9,34 +9,33 @@ public class BitStringUtils {
      * This function converts a bytearray into a bit string adding zero padding to the left. The size of the bit mask
      * is size 8.
      *
-     * This is a standard implementation of a toBitString function privided by
-     * http://www.java2s.com/Tutorials/Java/Data_Type/Array_Convert/Convert_byte_array_to_bit_string_in_Java.htm
-     *
      * @param byte[] to convert
      * @return bitstring representation of the passed byte[]
      */
-    public static String toBitString(final byte[] b) {
-        final char[] bits = new char[8 * b.length];
-        for(int i = 0; i < b.length; i++) {
-            final byte byteval = b[i];
-            int bytei = i << 3;
-            int mask = 0x1;
-            for(int j = 7; j >= 0; j--) {
-                final int bitval = byteval & mask;
-                if(bitval == 0) {
-                    bits[bytei + j] = '0';
-                } else {
-                    bits[bytei + j] = '1';
-                }
-                mask <<= 1;
-            }
+    public static String toBitString(final byte[] bytes) {
+        StringBuilder stringBuilder = new StringBuilder();
+        for (byte b : bytes) {
+            stringBuilder.append(toBits(b));
         }
-        return String.valueOf(bits);
+        return stringBuilder.toString();
     }
 
     /**
-     * Removes the last one and all subsequent zeros from a String.
+     * Create a bit string of length 8 from a byte.
      *
+     * This method masks the given byte using bit wise and with 0xFF. This means, only the last 8 bits will be
+     * considered when creating the string. This also makes sure that no bit string of length > 8 can be created.
+     *
+     * If the resulting bit string has a length of < 8 zero-padding is added on the left until the length is exactly 8.
+     */
+    private static String toBits(final byte val) {
+        return String.format("%8s", Integer.toBinaryString(val & 0xFF)).replace(' ', '0');
+    }
+
+
+    /**
+     * Removes the last one and all subsequent zeros from a String.
+     * <p>
      * This can be used to remove the suffix appended by BitStringUtils{@link #appendSuffix(StringBuilder)}
      *
      * @param bitString
@@ -50,7 +49,7 @@ public class BitStringUtils {
     /**
      * Appends a single one to a bit string. If the length of the resulting String is not dividible by 8 it
      * appends zeros until it is.
-     *
+     * <p>
      * This can be used to append a suffix to a bit string. Which will allow for it to be convieniently converted
      * to a byte array. Further the suffix can be used to indicate the bit strings end.
      *
