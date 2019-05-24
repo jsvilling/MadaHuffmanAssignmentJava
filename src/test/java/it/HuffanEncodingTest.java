ccc5a819bd0fd01478fb06e14378378b7351df2f
Joshua Villing <jsvilling@gmail.com>
2019-05-24 18:02:53 +0200

    Add integration test for encode decode cycle

--- src/test/java/it/HuffanEncodingTest.java
+++ src/test/java/it/HuffanEncodingTest.java
@@ -13,15 +13,15 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 public class HuffanEncodingTest {
 
-    private static String sourceFile = "src/test/resources/it/decde/text.txt";
-    private static String codeFile = "src/test/resources/it/decode/dec_tab-mada.txt";
-    private static String compressedFile = "src/test/resources/it/decode/output-mada.dat";
-    private static String decompressedFile = "src/test/resources/it/decode/text.txt";
-    private static String decompressedExpectedFile = "src/test/resources/it/decode/decompressedExpected.txt";
 
     @Test
     public void testDecodingEncodedFromExerciseSheet() throws IOException {
         // Given
+        String sourceFile = "src/test/resources/it/decde/text.txt";
+        String codeFile = "src/test/resources/it/decode/dec_tab-mada.txt";
+        String compressedFile = "src/test/resources/it/decode/output-mada.dat";
+        String decompressedFile = "src/test/resources/it/decode/text.txt";
+        String decompressedExpectedFile = "src/test/resources/it/decode/decompressedExpected.txt";
         FileService fileService = new FileService(sourceFile, codeFile, compressedFile, decompressedFile);
         EncodingService encodingService = new EncodingService(fileService, new CodeMapService());
 
@@ -41,7 +41,7 @@ public class HuffanEncodingTest {
         String codeFile = "src/test/resources/it/encode/dec_tab-mada.txt";
         String compressedFile = "src/test/resources/it/encode/output-mada.dat";
         String decompressedFile = "src/test/resources/it/encode/text.txt";
-        String codeFileExpected = "src/test/resources/it/encode/dec_tab-expected.txt";
+        String codeFileExpected = "src/test/resources/it/encode/dec_tab-mada.txt";
 
         FileService fileService = new FileService(sourceFile, codeFile, compressedFile, decompressedFile);
         EncodingService encodingService = new EncodingService(fileService, new CodeMapService());
@@ -57,4 +57,28 @@ public class HuffanEncodingTest {
         assertThat(actualCode).isEqualTo(expectedCode);
     }
 
+    @Test
+    public void testEncodingDecodingGivesOriginalText() throws IOException {
+        // Given
+        String sourceFile = "src/test/resources/it/cycle/text.txt";
+        String codeFile = "src/test/resources/it/cycle/dec_tab-mada.txt";
+        String compressedFile = "src/test/resources/it/cycle/output-mada.dat";
+        String decompressedFile = "src/test/resources/it/cycle/decompressed.txt";
+
+        Files.writeString(Paths.get(codeFile), "");
+        Files.writeString(Paths.get(compressedFile), "");
+        Files.writeString(Paths.get(decompressedFile), "");
+
+        // When
+        FileService fileService = new FileService(sourceFile, codeFile, compressedFile, decompressedFile);
+        EncodingService encodingService = new EncodingService(fileService, new CodeMapService());
+
+        // Then
+        encodingService.encode();
+        encodingService.decode();
+
+        String actualCode = Files.readString(Paths.get(decompressedFile));
+        String expectedCode = Files.readString(Paths.get(sourceFile));
+        assertThat(actualCode).isEqualToNormalizingWhitespace(expectedCode);
+    }
 }
